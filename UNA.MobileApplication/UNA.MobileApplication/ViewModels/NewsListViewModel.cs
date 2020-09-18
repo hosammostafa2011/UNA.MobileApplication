cb5490561a3f40b174775a1d57d2082acf3220e2
Hosam Mostafa <hosammostafa2011@gmail.com>
2020-09-18 22:17:09 +0300

    add vm

---
+++ UNA.MobileApplication/UNA.MobileApplication/ViewModels/NewsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UNA.MobileApplication.ViewModels
+{
+    public class NewsListViewModel: BaseViewModel
+    {
+    }
+}
--- UNA.MobileApplication/UNA.MobileApplication/Views/NewsList.xaml.cs
+++ UNA.MobileApplication/UNA.MobileApplication/Views/NewsList.xaml.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace UNA.MobileApplication.Views
@@ -17,11 +19,14 @@ namespace UNA.MobileApplication.Views
         {
             InitializeComponent();
             CategoryId = categoryID;
-            //load data by cat id
         }
         protected override void OnAppearing()
         {
+            //load data by cat CategoryId
             base.OnAppearing();
+            var safeInsets = On<iOS>().SafeAreaInsets();
+            safeInsets.Bottom = 0;
+            Padding = safeInsets;
         }
     }
 }
