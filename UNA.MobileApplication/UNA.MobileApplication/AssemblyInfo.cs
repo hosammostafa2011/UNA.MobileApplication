c0ebfe27f63465b8a273bb25239b3175fe39b04d
Hosam Mostafa <hosammostafa2011@gmail.com>
2020-09-29 19:14:07 +0300

    add font

--- UNA.MobileApplication/UNA.MobileApplication/AssemblyInfo.cs
+++ UNA.MobileApplication/UNA.MobileApplication/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("DroidKufiBold.ttf", Alias = "DroidKufiBold")]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+//[assembly: ExportFont("ArabotoLight.ttf", Alias = "Araboto Light")]
