146bc301787cb91fadb443a4a0b6ed8506af755c
Hosam Mostafa <hosammostafa2011@gmail.com>
2020-10-11 20:54:54 +0300

    line height

--- Helper/Model/CONTACT.cs
+++ Helper/Model/CONTACT.cs
@@ -2,7 +2,9 @@
 {
     public class CONTACT
     {
+        public string order_id;
+
         public string contact_title { get; set; }
-        public string contact_Email { get; set; }
+        public string email_val { get; set; }
     }
 }
