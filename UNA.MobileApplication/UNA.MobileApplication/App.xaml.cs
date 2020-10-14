828a3e3b065f1f1c11c022586e0160a0bd5e072b
Ahmad Soliman <asoliman@windowslive.com>
2020-10-14 18:48:57 +0300

    ios splash

--- UNA.MobileApplication/UNA.MobileApplication/App.xaml.cs
+++ UNA.MobileApplication/UNA.MobileApplication/App.xaml.cs
@@ -3,24 +3,31 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using UNA.MobileApplication.Services;
 using UNA.MobileApplication.Views;
+
 [assembly: ExportFont("HelveticaNeue.ttf", Alias = "HelveticaNeue")]
+
 namespace UNA.MobileApplication
 {
     public partial class App : Application
     {
-
         public App()
         {
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            if (Device.RuntimePlatform == Device.iOS)
+            {
+                MainPage = new SplashPage();
+            }
+            else if (Device.RuntimePlatform == Device.Android)
+            {
+                MainPage = new AppShell();
+            }
         }
 
         protected override void OnStart()
         {
             base.OnStart();
-            
         }
 
         protected override void OnSleep()
@@ -32,6 +39,5 @@ namespace UNA.MobileApplication
         {
             base.OnResume();
         }
-
     }
 }
