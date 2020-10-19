f2ea44c600d206d5af54c4f50f7ba7b01d7190c4
Ahmad Soliman <asoliman@windowslive.com>
2020-10-19 19:17:27 +0300

    splash

--- UNA.MobileApplication/UNA.MobileApplication.Android/MainActivity.cs
+++ UNA.MobileApplication/UNA.MobileApplication.Android/MainActivity.cs
@@ -16,7 +16,13 @@ using Android.Content;
 
 namespace UNA.MobileApplication.Droid
 {
-    [Activity(Label = "UNA-OIC (يونا)", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "UNA-OIC (يونا)",
+        Icon = "@drawable/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+         NoHistory = true
+        //ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize
+        )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
